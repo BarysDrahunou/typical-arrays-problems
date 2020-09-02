53b943008ebdaf4de5e0ecfd30bf714e85cb5975
Barys Drahunou <barysdrahunou@gmail.com>
2020-09-02 08:00:30 +0300

    commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
-
-exports.min = function min (array) {
-  return Math.min(array);
+exports.min = function min(array) {
+    check(array);
+    return Math.min(array);
 }
 
-exports.max = function max (array) {
+exports.max = function max(array) {
+    check(array);
     return Math.max(array)
 }
 
-exports.avg = function avg (array) {
-  return array.reduce((a,b)=>a+b,0)/array.length;
+exports.avg = function avg(array) {
+    check(array);
+    return array.reduce((a, b) => a + b, 0) / array.length;
+}
+
+function check(array) {
+    if (array === undefined || array.length === 0) return []
 }
